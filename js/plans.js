76ffeff983b48066460248a93281ff7906da005f
Brayden Alvarado <78436408+sweetcherrybeary@users.noreply.github.com>
2025-05-01 16:05:51 -0500

    Final adjustments

--- js/plans.js
+++ js/plans.js
@@ -160,13 +160,14 @@ document.addEventListener("DOMContentLoaded", function () {
             doc.text(`${originalTitle}`, x, y);
             y += 15;
         
-            doc.setFontSize(12);
+            doc.setFontSize(18);
             doc.text("Checklist:", 10, y);
             y += 8;
 
             // Split the checklist text into individual lines
             const checklistItems = checklistText.split("\n");
 
+            document.setFontSize(12);
             checklistItems.forEach(item => {
                 doc.text(item, 10, y);
                 y += 8;
